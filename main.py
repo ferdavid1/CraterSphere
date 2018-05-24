da2e0e3575247bb9c310f263487a9f81d6630ed5
ferdavid1 <fespinosa@ucdavis.edu>
2018-05-24 11:31:10 -0700

    small

--- main.py
+++ main.py
@@ -10,24 +10,22 @@ def neighborhood_dist(lat1, lon1, lat2, lon2, eps=eps):
 
 def main():
 	data = pd.read_csv("Data.csv")
-	data = data[['LON_E', 'LAT', 'DiamKM']][:1000]
+	data = data[['LON_E', 'LAT', 'DiamKM']][:5000]
 	lon, lat, diam = data['LON_E'], data['LAT'], data['DiamKM']
-	# G = nx.MultiGraph()
+	G = nx.MultiGraph()
 	zipped = list(zip(lat, lon))
-	# G.add_nodes_from(zipped[:1000])
-	plt.scatter(lat, lon)
+	G.add_nodes_from(zipped)
 	inds = []
 	for ind, (x,y) in enumerate(zipped):
 		for x2,y2 in zipped:
-			if neighboohood_dist(x,y,x2,y2):
-				# G.add_edge((x,y), (x2,y2))
-				plt.plot([x, x2], [y, y2], color='k', linestyle='-')
-				# inds.append(ind)
+			if neighborhood_dist(x,y,x2,y2) and (x,y) != (x2,y2):
+				G.add_edge((x,y), (x2,y2))
+				inds.append(ind)
 			else:
 				pass
-	# diam = [diam[ind] for ind in inds]
+	diam = [diam[ind] for ind in inds]
 	# print(len(list(G.edges())))
-	# nx.draw(G, node_size=diam, with_labels=True)
+	nx.draw(G, node_size=diam, with_labels=False)
 	plt.show()
 
 main()
